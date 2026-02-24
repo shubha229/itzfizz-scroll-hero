d9b821ba96cb340afde3353154f49179b1df8741
shubha229 <shubhashreenayak9825@gmail.com>
2026-02-24 19:09:49 +0530

    fix basePath for production

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
-  }
+  },
+  basePath: isProd ? "/itzfizz-scroll-hero" : "",
+  assetPrefix: isProd ? "/itzfizz-scroll-hero/" : "",
 };
 
 export default nextConfig;
